7d089a344d4563a59ee54aef80bd66c76d292a5a
tafiran <misha.zaharov.2017@mail.ru>
2021-09-15 03:52:41 +0300

    ;l

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/style.css">
+
+    <title class="lg">Title</title>
+</head>
+
+<body>
+    <div class="block" id="menu">
+        <div class="logo-part">
+            <img src="assets/media/logo.svg" alt="log" class="logo-img">
+            <div class="logo-text">ЛАБЫ</div>
+        </div>
+        <ul class="menu-nav">
+            <li class="menu-nav_element">Главная</li>
+            <li class="menu-nav_element">Услуги</li>
+            <li class="menu-nav_element">Гарантия</li>
+            <li class="menu-nav_element">Образцы решений</li>
+            <li class="menu-nav_element">Контакты</li>
+            <li><div class="button sign-up">Авторизация</div></li>
+        </ul>
+        
+        
+    </div>
+    <a class="navicon-button">
+      <div class="navicon"></div>
+    </a>
+    <div class="navicon" type="sqareFour" id="third"></div>
+</body>
+
+<footer>
+    <?php include 'assets/scripts.php' ?>
+</footer>
+
+</html>
